ae19401d2abda96b82e88c354fbcfc3fc3c6df4f
Chaney <lh-chan@mechapilot.org>
2019-01-12 18:16:29 -0500

    Fix?

--- dato.config.rb
+++ dato.config.rb
@@ -35,7 +35,7 @@ create_post "src/about.md" do
   frontmatter :yaml, {
     title: dato.about.title,
     image: dato.about.image.url,
-    latitude: dato.about.position.latitude,
+    latitude: dato.about.location.latitude,
     layout: 'about',
     permalink: /about/,
     seo_settings: dato.about.seo_settings,

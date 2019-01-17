8990a3ac4663e9ed875a1dc0ae08db732e26cd6c
Chaney <lh-chan@mechapilot.org>
2019-01-17 17:21:19 -0500

    Test JS at page bottom.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -14,14 +14,3 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
   });
 });
-
-jQuery.jQueryRandom = 0;
-jQuery.extend(jQuery.expr[":"],
-{
-    random: function(a, i, m, r) {
-        if (i == 0) {
-            jQuery.jQueryRandom = Math.floor(Math.random() * r.length);
-        };
-        return i == jQuery.jQueryRandom;
-    }
-});
